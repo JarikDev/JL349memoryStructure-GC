30a1fb256077faa779ceff2bd53817a0d68837f5
ivan4000 <sxln1@mail.ru>
2018-12-06 18:49:17 +0300

    JL349 memory structure and CG

---
+++ src/com/Main.java
@@ -0,0 +1,33 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        doJob(5, new Object());
+    }
+    private static int doJob(int i, Object o) {
+        return i;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
